d7f09b364f50ded127e70a55d66bea1a82808529
Gerald Mixa <gerald@mixas.de>
2022-11-04 21:55:11 +0100

    use case: Comparator Subclass is Attribute

--- src/test/java/tests/comparatorAsAttribute/ComparatorIsAttribute.java
+++ src/test/java/tests/comparatorAsAttribute/ComparatorIsAttribute.java
@@ -1,10 +1,18 @@
 package tests.comparatorAsAttribute;
 
+import tests.MyIntComparator;
+
 import java.util.Comparator;
 
 public class ComparatorIsAttribute {
 
     private Comparator<Integer> comparatorAttribute;
+
+    private MyIntComparator myIntComparator;
+
+    public void setMyIntComparator(MyIntComparator myIntComparator) {
+        this.myIntComparator = myIntComparator;
+    }
 
     public void setComparatorAttribute(Comparator<Integer> comparatorAttribute){
         this.comparatorAttribute = comparatorAttribute;
