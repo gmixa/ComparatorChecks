544b39111da5de581aefedc1b234342bb8f11c02
Gerald Mixa <gerald@mixas.de>
2022-11-05 13:32:07 +0100

    Add ReadMe.md

---
+++ ReadMe.md
@@ -0,0 +1,4 @@
+[![Scala CI](https://github.com/gmixa/ComparatorChecks/actions/workflows/scala.yml/badge.svg)](https://github.com/gmixa/ComparatorChecks/actions/workflows/scala.yml)
+
+This project aims to test java comparators and compareables automatically for their 
+correctness.
--- src/main/scala/de/mixas/comparatorchecks/classgraph/Scanner.scala
+++ src/main/scala/de/mixas/comparatorchecks/classgraph/Scanner.scala
@@ -23,7 +23,6 @@ private class Scanner(packageName : String):
        val clazz = classes.loadClasses(classOf[Comparable[_]])
        clazz.asScala.toSet
   }
-
   def allComparators(): Try[Set[Class[_ <: Comparator[_]]]] = using{
     scanResult =>
       val classes = scanResult.getClassesImplementing(classOf[Comparator[_]])
@@ -35,10 +34,11 @@ private class Scanner(packageName : String):
     scanResult =>
       val fields: List[Field] = for {
         classes <- scanResult.getAllClasses.asScala.toList
-        fields <- classes.getFieldInfo.asScala.toList
+        field <- classes.getFieldInfo.asScala.toList
       } yield
-        println(fields)
-        fields.loadClassAndGetField()
+        println(field)
+        println(field.getTypeDescriptor)
+        field.loadClassAndGetField()
       fields.toSet
   }
 
@@ -46,6 +46,5 @@ object Scanner:
   def apply(packageName : String) : Scanner =
     require(packageName!=null,"null no legal package name")
     new Scanner(packageName)
-
 end Scanner
 
